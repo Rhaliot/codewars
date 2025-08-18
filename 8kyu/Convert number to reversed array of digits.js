f96a5036777f651dc51bc052ffff8bb33ef99168
Elliot <filipelliotkozuchowski@proton.me>
2025-08-18 16:07:18 +0200

    add 8kyu

---
+++ 8kyu/Convert number to reversed array of digits.js
@@ -0,0 +1,12 @@
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+// Example (Input => Output):
+
+// 35231 => [1,3,2,5,3]
+// 0     => [0]
+
+// My solution:
+
+function digitize(n) {
+  let sum = n.toString().split('').reverse().map(Number);
+  return sum;
+}
