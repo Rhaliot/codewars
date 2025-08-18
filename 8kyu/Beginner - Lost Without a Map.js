66b13a48fa304f9f87d5f4d63c2af00e5ab698ce
Elliot <filipelliotkozuchowski@proton.me>
2025-08-18 16:12:10 +0200

    add 8 kyu

---
+++ 8kyu/Beginner - Lost Without a Map.js
@@ -0,0 +1,12 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+// My solution:
+
+function maps(x){
+ let doubledArray = x.map(n => n * 2)
+ return doubledArray;
+}
