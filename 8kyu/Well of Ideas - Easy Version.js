26bca8e7903cd7cbc91d0994121b5f44e5a87371
Elliot <filipelliotkozuchowski@proton.me>
2025-08-22 14:33:01 +0200

    add 8kyu

---
+++ 8kyu/Well of Ideas - Easy Version.js
@@ -0,0 +1,21 @@
+// For every good kata idea there seem to be quite a few bad ones!
+
+// In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+// My solution:
+
+function well(x) {
+  const result = x.reduce((acc, curr) => {
+    if (curr === "good") {
+      acc++;
+    }
+    return acc;
+  }, 0);
+
+  if (result == 2 || result == 1) {
+    return "Publish!";
+  } else if (result >= 3) {
+    return "I smell a series!";
+  }
+  return "Fail!";
+}
