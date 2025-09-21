498108244d625a4769ce9493bbd79295349e45d8
Elliot <filipelliotkozuchowski@proton.me>
2025-09-21 19:34:43 +0200

    fix solution

--- 8kyu/The Feast of Many Beasts.js
+++ 8kyu/The Feast of Many Beasts.js
@@ -8,7 +8,8 @@
 
 function feast(beast, dish) {
   if (
-    (dish[0] && dish[dish.length - 1]) === (beast[0] && beast[beast.length - 1])
+    dish[0] === beast[0] &&
+    dish[dish.length - 1] === beast[beast.length - 1]
   ) {
     return true;
   }
